2c39b4ddd98cc25a8c9bb51daa41a6dd44b083d0
Nyaasu <nyaasu@qq.com>
2020-08-04 13:56:54 +0800

    🔖 Version-2.8.1

--- CSDN-Beautify-latest.user.js
+++ CSDN-Beautify-latest.user.js
@@ -7,7 +7,7 @@
 // @description:zh-CN   还你一个经典版的CSDN：作者信息和顶部导航栏保留，去掉右边评论区；内容自动展开；去广告；屏宽自动适配；净化剪贴板。
 
 // @namespace    https://github.com/z1064244797/CSDN-Beautify
-// @version      2.7.4
+// @version      2.8.1
 // @author       Nyaasu
 // @match        *://blog.csdn.net/*/article/details/*
 // @run-at       document-end
@@ -21,25 +21,28 @@
     'use strict';
     //阅读全文
     $('.hide-article-box').remove();
-    $('#article_content').css({'height':'initial'});
+    $('#article_content').css({
+        'height': 'initial'
+    });
     //vip免广告 按钮
-    $('.meau-gotop-box').remove();
+    // $('.meau-gotop-box').remove();
     //未登录提示
-    $('.unlogin-box').remove();
+    // $('.unlogin-box').remove();
     //去除剪切板劫持
     csdn.copyright.init("", "", "");
     //移除左侧最新评论
-    $('#asideNewComments').remove();
+    // $('#asideNewComments').remove();
     //移除左侧CSDN联系方式
-    $('.persion_article').remove();
+    // $('.persion_article').remove();
     //移除右侧工具栏
-    $('.tool-box').remove()
+    // $('.tool-box').remove()
     //下部推荐
-    $('.recommend-box').remove();
+    // $('.recommend-box').remove();
     //取消注册登录弹出
     $('#passportbox').remove();
     $('.login-mark').remove();
     //两栏处理
+    $('.blog_container_aside').remove();
     // $('.nodata .container').css({'width':'1318px !important'})
     // $('.nodata .tool-box .meau-list .btn-like-box p').css({'display': 'block'})
     // $('.recommend-right').css({'display':'none'})
@@ -53,43 +56,86 @@
     // $('body').css({'min-width':'0'});
     // $('.csdn-toolbar').css({'min-width':'0'});
     //去广告
-    $('.pulllog-box').remove();
-    $('.fourth_column').remove();
-    $('.mb8').remove();
-    $('newsfeed').remove();
-    $('#asideFooter').remove();
-    $("li:contains('赚零钱')").remove();
-    $('.light-box').remove();
-    $("a[data-type='vip']").remove();
-    $(".csdn-side-toolbar").remove();
-    $(".vip-caise").remove();
-    $("iframe").remove();
+    // $('.pulllog-box').remove();
+    // $('.fourth_column').remove();
+    // $('.mb8').remove();
+    // $('newsfeed').remove();
+    // $('#asideFooter').remove();
+    $("li:contains('VIP会员')").remove();
+    // $('.light-box').remove();
+    // $("a[data-type='vip']").remove();
+    // $(".csdn-side-toolbar").remove();
+    // $(".vip-caise").remove();
+    $('.csdn-tracking-statistics').remove();
+    $('#addAdBox').remove();
+    $('#recommendAdBox').remove();
+    setTimeout(() => {
+        $('#groupfile').css({
+            'max-height': '80%'
+        });
+        $('#footerRightAds').remove();
+        console.log('56565')
+        $('[data-title="登录后复制"]').remove();
+        $("main").css({
+            "float": "none",
+            "margin": "0 auto"
+        });
+        resize();
+    }, 800)
+    // 屏宽适配
 
-    //屏宽适配
-    // function resize(){
-    //     var result1 = window.matchMedia('(min-width:1360px)');
-    //     var result2 = window.matchMedia('(min-width:1120px)');
-    //     if (result1.matches) {
-    //         console.log("大屏宽，恢复原样式");
-    //         $("aside").css({'display':'block'});
-    //         $("#mainBox > main").css("width","1010px");
-    //         $('.container').css({'width':'1318px'})
-    //     } else if (result2.matches) {
-    //         console.log("中屏宽，减小mainBox宽度");
-    //         $("aside").css({'display':'block'});
-    //         $('.container').css({'width':'1178px'})
-    //         $("#mainBox > main").css("width","870px");
-    //     } else {
-    //         console.log("小屏宽，隐藏右侧信息栏，mainBox居中");
-    //         $("aside").css({'display':'none'});
-    //         $('.container').css({'width':'910px'});
-    //         $("#mainBox > main").css("width","870px");
-    //         $(".btns").css("float","right");
-    //         $(".csdn-toolbar > div").css("width","870px");
-    //     }
-    // }
-    // window.onresize=function() {resize()}
-    // resize();
+    function resize() {
+        var result1 = window.matchMedia('(min-width:1220px)');
+        var result2 = window.matchMedia('(min-width:1020px)');
+        if (result1.matches) {
+            console.log("大屏宽，恢复原样式");
+            $("aside").css({
+                'display': 'block'
+            });
+            $("main").css({
+                "width": "1110px",
+            });
+            $('.container').css({
+                'width': '1418px'
+            })
+            $(".main_father").css({
+                "width": "100%",
+            });
+        } else if (result2.matches) {
+            console.log("中屏宽，减小mainBox宽度");
+            $("aside").css({
+                'display': 'block'
+            });
+            $('.container').css({
+                'width': '1008px'
+            })
+            $("main").css({
+                "width": "970px",
+            });
+            $(".main_father").css({
+                "width": "1070px",
+            });
+        } else {
+            console.log("小屏宽，隐藏右侧信息栏，mainBox居中");
+            $("aside").css({
+                'display': 'none'
+            });
+            $('.container').css({
+                'width': '820px'
+            });
+            $("main").css({
+                "width": "800px",
+            });
+            $(".main_father").css({
+                "width": "1010px",
+            });
+            $(".btns").css("float", "right");
+            $(".csdn-toolbar > div").css("width", "870px");
+        }
+    }
+    window.onresize = function () {
+        resize()
+    }
     // //显示评论区
     // $('.comment-list-box').css({'display':'block'});
     // $('.comment-list-box').css({'max-height':'870px'});
