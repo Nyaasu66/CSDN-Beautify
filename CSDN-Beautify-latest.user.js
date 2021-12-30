0f9877d41a93ba06abcb08a3a7a5c7edc1e89d6c
Nyaasu <nyaasu@qq.com>
2021-12-30 15:57:09 +0800

    🔖  Version-2.9.3

--- CSDN-Beautify-latest.user.js
+++ CSDN-Beautify-latest.user.js
@@ -7,7 +7,7 @@
 // @description:zh-CN   还你一个经典版的CSDN：作者信息和顶部导航栏保留，去掉右边评论区；内容自动展开；去广告；屏宽自动适配；净化剪贴板。
 
 // @namespace    https://github.com/z1064244797/CSDN-Beautify
-// @version      2.9.2
+// @version      2.9.3
 // @author       Nyaasu
 // @match        http*://blog.csdn.net/*/article/details/*
 // @match        http*://csdnnews.blog.csdn.net/article/details/*
@@ -62,6 +62,7 @@
     // $('.mb8').remove();
     // $('newsfeed').remove();
     // $('#asideFooter').remove();
+    $('#passport-login-container').remove();
     $("li:contains('VIP会员')").remove();
     // $('.light-box').remove();
     // $("a[data-type='vip']").remove();
